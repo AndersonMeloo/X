f8bc2becace9300043d09a0dc22c00d7e19250c6
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-10 00:08:59 -0300

    feat: novos imports das pastas. style: tailwind

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,3 +1,9 @@
+import { NavItem } from "@/components/nav/nav-item"
+import { NavLogout } from "@/components/nav/nav-logout"
+import { NavMyProfile } from "@/components/nav/nav-myprofile"
+import { Logo } from "@/components/ui/logo"
+import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons"
+
 type Props = {
     children: React.ReactNode
 }
@@ -10,7 +16,29 @@ export default function Layout({ children }: Props) {
             <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
 
                 <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
-                    Esquerda
+                    <div className="flex-1 mt-6">
+                        <Logo size={24} />
+                        <nav className="mt-11">
+
+                            <NavItem
+                                href="/home"
+                                icon={faHouse}
+                                label="Página inicial"
+                            />
+
+                            <NavItem
+                                href="/profile"
+                                icon={faUser}
+                                label="Meu Perfil"
+                            />
+                        </nav>
+                    </div>
+
+                    <div className="mb-6 flex flex-col gap-4">
+                        <NavLogout />
+                        <NavMyProfile />
+                    </div>
+
                 </section>
 
                 <section className="flex-1 max-w-lg">
