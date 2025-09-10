d51673dde648f4395ed0be471aa61f230e03181c
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-10 00:10:41 -0300

    feat: nova rota navigate

--- src/components/auth/signin-form.tsx
+++ src/components/auth/signin-form.tsx
@@ -3,16 +3,42 @@
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
 export const SigninForm = () => {
 
     const router = useRouter()
-    const [emailField, setEmailField] = useState()
-    const [passwordField, setPasswordField] = useState()
+    const [emailField, setEmailField] = useState('')
+    const [passwordField, setPasswordField] = useState('')
+
+    const handleEnterButton = () => {
+        router.replace('/home')
+    }
 
     return (
 
         <>
-            <input placeholder="Digite seu e-mail"/>
+            <Input
+                placeholder="Digite seu e-mail"
+                value={emailField}
+                onChange={text => setEmailField(text)}
+            />
+
+            <Input
+                placeholder="Digite sua senha"
+                value={passwordField}
+                onChange={text => setPasswordField(text)}
+                password
+            />
+
+            <Button
+                label="Entrar"
+                onClick={handleEnterButton}
+                size={1}
+            />
         </>
     )
 }
