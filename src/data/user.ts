b0dad0b5e14a6af6c5121b4f54c39fc35bc09071
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-11 23:20:46 -0300

    feat: trocando name

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,7 +3,7 @@ import { User } from "@/types/user";
 export const user: User = {
 
     slug: 'Anderson',
-    name: 'Anderson Rodrigues de Melo',
+    name: 'Anderson Melo',
     avatar: 'https://ui-avatars.com/api/?background=random&name=Anderson&size=100',
     cover: 'https://th.bing.com/th/id/R.6cb616c880ca291582ab569d443f9cc6?rik=niud7c7LmWUC2Q&pid=ImgRaw&r=0',
     bio: 'Desenvolvedor Front-End',
