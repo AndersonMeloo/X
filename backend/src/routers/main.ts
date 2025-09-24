a17a009229dc3df09ab7945572b9ed231b63f873
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-23 23:42:08 -0300

    feat: Creating all routes that will be necesasry

--- backend/src/routers/main.ts
+++ backend/src/routers/main.ts
@@ -1,8 +1,48 @@
 import { Router } from 'express'
 
 import * as pingController from '../controllers/ping'
+import * as authController from '../controllers/auth'
 
 export const mainRouter = Router()
 
 // Testando Servidor
 mainRouter.get('/ping', pingController.ping)
+// mainRouter.get('/privatePing')
+
+// Register
+mainRouter.post('/auth/signup', authController.signup)
+// Login
+// mainRouter.post('/auth/signin')
+
+// Create Tweet
+// mainRouter.post('./tweet')
+// Pegar um Tweet
+// mainRouter.get('/tweet/:id')
+// Repostas do Tweet
+// mainRouter.get('/tweet/:id/answers')
+// Likes Tweet
+// mainRouter.post('/tweet/:id/like')
+
+// Infos do Usuarios
+// mainRouter.get('/user/:slug')
+// Tweet dos Usuarios
+// mainRouter.get('/user/:slug/tweets')
+// Seguir / Deixar de Seguir
+// mainRouter.post('/user/:slug/follow')
+
+// Alterar Dados do Usuario
+// mainRouter.put('/user')
+// Alterar Avatar
+// mainRouter.put('/user/avatar')
+// Alterar Capa
+// mainRouter.put('/user/cover')
+
+// Feeds
+// mainRouter.get('/feed')
+// Pesquisar
+// mainRouter.get('/search')
+// Trending #
+// mainRouter.get('/trending')
+
+// Sugestões para seguir
+// mainRouter.get('/seggestions')
