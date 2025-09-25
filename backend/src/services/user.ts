47a0fb4673c06b81c2d6415455a73ef15fa81fca
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-24 23:38:31 -0300

    feat: criando routes para criar user

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,9 +1,10 @@
+import { Prisma } from "../generated/prisma"
 import { prisma } from "../utils/prisma"
 import { getPublicURL } from "../utils/url"
 
 export const findUserByEmail = async (email: string) => {
 
-    const user = await prisma.findFirst({
+    const user = await prisma.user.findFirst({
 
         where: { email }
     })
@@ -21,7 +22,7 @@ export const findUserByEmail = async (email: string) => {
 
 export const findUserBySlug = async (slug: string) => {
 
-    const user = await prisma.url.findFirst({
+    const user = await prisma.user.findFirst({
 
         select: {
             avatar: true,
@@ -43,4 +44,15 @@ export const findUserBySlug = async (slug: string) => {
     }
 
     return null
+}
+
+export const createUser = async (data: Prisma.UserCreateInput) => {
+
+    const newUser = await prisma.user.create({ data })
+
+    return {
+        ...newUser,
+        avatar: getPublicURL(newUser.avatar),
+        cover: getPublicURL(newUser.cover)
+    }
 }
