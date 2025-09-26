71841068213dbadb93ed8b2fdcd85ff8634cbda9
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-25 23:51:10 -0300

    feat: Adding pingPrivate that Will Only Work for Logged-In Users

--- backend/src/controllers/ping.ts
+++ backend/src/controllers/ping.ts
@@ -1,6 +1,12 @@
 import { Request, RequestHandler, Response } from "express";
+import { ExtendedRequest } from "../types/extended-request";
 
 export const ping = (req: Request, resp: Response) => {
 
     resp.json({ pong: true })
+}
+
+export const privatePing = (req: ExtendedRequest, resp: Response) => {
+
+    resp.json({ pong: true, slug: req.userSlug })
 }
