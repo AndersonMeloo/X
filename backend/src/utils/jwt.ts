61ac3f2611b15fac6304d0fc7f589fe39a1f7691
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-25 23:46:10 -0300

    feat: New JWT FILE for Login

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,7 +1,34 @@
+import { NextFunction, Request, Response } from "express"
 import jwt from "jsonwebtoken"
+import { findUserBySlug } from "../services/user"
+import { ExtendedRequest } from "../types/extended-request"
 
 // JSON WEB TOKEN
 export const createJWT = (slug: string) => {
 
     return jwt.sign({ slug }, process.env.JWT_SECRET as string)
+}
+
+export const verifyJWT = (req: ExtendedRequest, res: Response, next: NextFunction) => {
+
+    // return res.status(401).json({ error: 'Bloquendo a Rota pra Teste' })
+
+    const authHeader = req.headers['authorization']
+    if (!authHeader) return res.status(401).json({ error: 'Acesso negado' })
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(
+        token,
+        process.env.JWT_SECRET as string,
+        async (error, decoded: any) => {
+            if (error) return res.status(401).json({ error: 'Acesso Negado' })
+
+            const user = await findUserBySlug(decoded.slug)
+            if (!user) return res.status(401).json({ error: 'Acesso Negado' })
+
+            req.userSlug = user.slug
+            next()
+        }
+    )
 }
