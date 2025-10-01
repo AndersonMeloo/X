6bec43cd23ca47136ab1542918bda5d426c5cfd3
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-10-01 01:28:12 -0300

    feat: fixing components/auth/signin-form, utils/api.ts packages app/ui/layout.tsx. style: components ui/button.tsx

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ export const Button = ({ label, onClick, size, type = "button" }: Props) => {
             <button
                 type={type}
                 onClick={onClick}
-                className={`flex justify-center items-center cursor-pointer bg-white text-black font-bold rounded-3xl
+                className={`flex justify-center items-center cursor-pointer bg-white text-black font-bold rounded-3xl p-4
                 ${size === 1 && 'h-14 text-lg'}
                 ${size === 2 && 'h-10 text-md'}
                 ${size === 3 && 'h-7 text-xs'}`}>
