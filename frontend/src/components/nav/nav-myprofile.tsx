9dc344f51f3e6385eed553868b101616196e8d09
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-10-01 23:31:03 -0300

    feat: import useAuthUser

--- frontend/src/components/nav/nav-myprofile.tsx
+++ frontend/src/components/nav/nav-myprofile.tsx
@@ -1,27 +1,40 @@
-import { user } from "@/data/user"
+"use client"
 import Link from "next/link"
+import { useAuthUser } from "@/hooks/useAuth"
 
 export const NavMyProfile = () => {
 
+    const { userData, loading } = useAuthUser()
+
+    if (loading) return <div>Carregando...</div>
+    if (!userData) return <div>Erro ao carregar usuário</div>
+
+    // Desestruturando o usuário para facilitar o acesso
+    const { user } = userData
+
     return (
-        <>
-            <div className="flex items-center">
-                <div className="size-10 mr-2 rounded-full overflow-hidden">
-                    <Link href={`/${user.slug}`}>
-                        <img />
+        <div className="flex items-center">
+            
+            {/* Avatar */}
+            <div className="w-10 h-10 mr-2 rounded-full overflow-hidden">
+                <Link href={`/${user.slug}`}>
+                    <img
                         src={user.avatar}
                         alt={user.name}
-                        className="size-full"
-                    </Link>
-                </div>
+                        className="w-full h-full object-cover"
+                    />
+                </Link>
+            </div>
 
-                <div className="flex-1 overflow-hidden">
-                    <Link href={`${user.slug}`} className="block truncate">
-                        {user.name}
-                    </Link>
-                    <div className="truncate text-sm text-gray-400">@{user.slug}</div>
+            {/* Nome e slug */}
+            <div className="flex-1 min-w-0">
+                <Link href={`/${user.slug}`} className="block truncate font-medium">
+                    {user.name}
+                </Link>
+                <div className="truncate text-sm text-gray-400">
+                    @{user.slug}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
