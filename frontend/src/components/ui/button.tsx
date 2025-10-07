646649d80a55fc560d215acea87c3f172f658914
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-10-07 00:04:30 -0300

    feat: nova tipagem

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -2,10 +2,11 @@ type Props = {
     label: string,
     onClick?: () => void,
     size: 1 | 2 | 3,
-    type?: "button" | "submit" | "reset"
+    type?: "button" | "submit" | "reset",
+    disable?: boolean
 }
 
-export const Button = ({ label, onClick, size, type = "button" }: Props) => {
+export const Button = ({ label, onClick, size, type = "button", disable }: Props) => {
 
     return (
 
@@ -13,6 +14,7 @@ export const Button = ({ label, onClick, size, type = "button" }: Props) => {
             <button
                 type={type}
                 onClick={onClick}
+                disabled={disable}
                 className={`flex justify-center items-center cursor-pointer bg-white text-black font-bold rounded-3xl p-4
                 ${size === 1 && 'h-14 text-lg'}
                 ${size === 2 && 'h-10 text-md'}
