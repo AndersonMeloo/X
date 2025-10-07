427448dfcdf8ea7613d5a75e6eefa50c567e859a
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-10-07 00:10:29 -0300

    feat: arrumando zod validar o frontend a alteração de usuario

--- backend/src/schemas/update-user.ts
+++ backend/src/schemas/update-user.ts
@@ -4,5 +4,9 @@ export const updateUserSchema = z.object({
 
     name: z.string().min(2, 'Precisa ter 2 ou mais caracteres').optional(),
     bio: z.string().optional(),
-    link: z.string().url('Precisa ser uma URL válida').optional()
+    link: z
+        .string()
+        .url('Precisa ser uma URL válida')
+        .optional()
+        .or(z.literal('')) // Permite que o Campo seja Vazio
 })
